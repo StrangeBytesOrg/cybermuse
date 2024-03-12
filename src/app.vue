bd7aa9475c2a6893f973bd90cd958ec411422534
StrangeBytesDev <dev@strangebytes.org>
2024-03-11 18:13:56 -0700

    Move styles into styles folder. Improve top level styles.

--- src/app.vue
+++ src/app.vue
@@ -1,14 +1,8 @@
 <script lang="ts" setup>
-import {ref} from 'vue'
 import {RouterLink, RouterView} from 'vue-router'
 import {useConnectionStore} from './store'
 
-const showMenu = ref(false)
 const connectionStore = useConnectionStore()
-
-const toggleMenu = () => {
-    showMenu.value = !showMenu.value
-}
 
 const checkConnection = async () => {
     // const connectionCheckUrl = `${connectionStore.apiUrl}/health`
@@ -42,33 +36,42 @@ const checkConnection = async () => {
 }
 checkConnection()
 
-import './assets/css/global.css'
-import './assets/css/tailwind.css'
+import './styles/global.css'
+import './styles/tailwind.css'
 </script>
 
 <template>
     <!-- Content -->
     <div class="flex flex-col flex-grow">
         <!-- Header -->
-        <div class="navbar w-full bg-neutral text-neutral-content fixed z-[50] top-0 flex justify-between">
-            <button class="btn btn-square btn-outline" aria-label="menu">
-                <img class="w-8" src="./assets/img/menu.svg" />
-            </button>
-        </div>
+        <div class="navbar w-full bg-neutral text-neutral-content fixed z-[50] top-0 flex justify-between"></div>
 
         <div class="flex flex-grow min-h-0 pt-16">
             <!-- Menu -->
-            <div
-                @click="toggleMenu"
-                class="wat-menu w-80 max-md:min-w-[20rem] max-md:w-1/2 flex flex-col bg-base-300"
-                :class="{show: showMenu}">
+            <!-- <div class="wat-menu w-80 max-md:min-w-[20rem] max-md:w-1/2 flex flex-col bg-base-300">
                 <div class="px-5 flex flex-col flex-grow">
                     <router-link to="/create-character">Create Character</router-link>
                     <router-link to="/characters">Characters</router-link>
                     <router-link to="/connection">Connection</router-link>
                     <router-link to="/settings">Settings</router-link>
                 </div>
-            </div>
+            </div> -->
+            <ul class="menu w-60 bg-base-200 min-h-full">
+                <li>
+                    <router-link to="/create-character" class="font-bold" active-class="active">
+                        Create Character
+                    </router-link>
+                </li>
+                <li>
+                    <router-link to="/characters" class="font-bold" active-class="active">Characters</router-link>
+                </li>
+                <li>
+                    <router-link to="/connection" class="font-bold" active-class="active">Connection</router-link>
+                </li>
+                <li>
+                    <router-link to="/settings" class="font-bold" active-class="active">Settings</router-link>
+                </li>
+            </ul>
 
             <!-- Main -->
             <div class="flex flex-col flex-1 overflow-auto md:mr-2">
