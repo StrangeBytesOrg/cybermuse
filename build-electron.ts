85c8970504f455d5c7cf5c6b6a0711425685cbce
StrangebytesDev <dev@strangebytes.org>
2024-06-16 15:59:37 -0700

    Update build CI for electron

--- build-electron.ts
+++ build-electron.ts
@@ -7,8 +7,8 @@ const dev = Boolean(process.env.DEV)
 
 const artifacts = await builder.build({
     config: {
-        buildNumber: process.env.BUILD_NUMBER || 'manual',
-        appId: 'chat-app',
+        // buildNumber: process.env.BUILD_NUMBER || 'manual',
+        appId: 'chat',
         directories: {
             output: 'out',
         },
@@ -19,13 +19,15 @@ const artifacts = await builder.build({
             {from: './src/migrations/', to: './migrations/'},
         ],
         linux: {
-            target: dev ? [{target: 'zip', arch: 'x64'}] : 'dir',
-            // asar: false,
+            target: dev ? 'dir' : [{target: 'zip', arch: 'x64'}],
             extraResources: [{from: './build/llamacpp/llama-server', to: 'llamacpp/llama-server'}],
         },
         win: {
             target: ['zip'],
-            extraResources: [{from: './build/llamacpp/llama-server.exe', to: 'llamacpp/llama-server.exe'}],
+            extraResources: [
+                {from: './build/llamacpp/llama-server.exe', to: 'llamacpp/llama-server.exe'},
+                {from: './build/llamacpp/llama.dll', to: 'llamacpp/llama.dll'},
+            ],
         },
         mac: {
             target: [
@@ -34,7 +36,7 @@ const artifacts = await builder.build({
             ],
             extraResources: [{from: './build/llamacpp/llama-server', to: 'llamacpp/llama-server'}],
         },
-        artifactName: '${productName}-${os}-${arch}-${buildNumber}.${ext}',
+        artifactName: 'chat-${os}-${arch}.${ext}',
         // TODO implement fuse flipping correctly for all platforms
         // afterPack: async ({appOutDir, packager}) => {
         //     const {productFilename} = packager.info.appInfo
