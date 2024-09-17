d4d68314f11089e2bca2eb5c6fdfc1b92619c306
StrangeBytesDev <dev@strangebytes.org>
2024-09-17 16:59:37 -0700

    Get latest llama bin from my bin's repo

--- get-llamacpp.sh
+++ get-llamacpp.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
+LLAMA_CPP_VERSION="b3778"
 
-LLAMA_CPP_VERSION="b3181"
+# curl -L "https://github.com/ggerganov/llama.cpp/releases/download/${LLAMA_CPP_VERSION}/llama-${LLAMA_CPP_VERSION}-bin-ubuntu-x64.zip" -o /tmp/llamacpp.zip
+# unzip -j /tmp/llamacpp.zip -d llamacpp
 
-curl -L "https://github.com/ggerganov/llama.cpp/releases/download/${LLAMA_CPP_VERSION}/llama-${LLAMA_CPP_VERSION}-bin-ubuntu-x64.zip" -o /tmp/llamacpp.zip
-unzip -j /tmp/llamacpp.zip -d llamacpp
+curl -L "https://github.com/StrangeBytesOrg/llama.cpp-binaries/releases/download/${LLAMA_CPP_VERSION}/llama-server-cpu.tar.gz" -o /tmp/llamacpp.tar.gz
+tar -xf /tmp/llamacpp.tar.gz -C llamacpp
