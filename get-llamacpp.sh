bd9721cb7d27a925ea65553b566c05c3e3f899b4
StrangebytesDev <dev@strangebytes.org>
2024-10-21 14:48:23 -0700

    Switch back to pulling llama.cpp from github and pull latest

--- get-llamacpp.sh
+++ get-llamacpp.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
-LLAMA_CPP_VERSION="b3812"
+LLAMA_CPP_VERSION="b3952"
 
 # Get a binary from the llama.cpp repo directly
-# curl -L "https://github.com/ggerganov/llama.cpp/releases/download/${LLAMA_CPP_VERSION}/llama-${LLAMA_CPP_VERSION}-bin-ubuntu-x64.zip" -o /tmp/llamacpp.zip
-# unzip -j /tmp/llamacpp.zip -d llamacpp
+curl -L "https://github.com/ggerganov/llama.cpp/releases/download/${LLAMA_CPP_VERSION}/llama-${LLAMA_CPP_VERSION}-bin-ubuntu-x64.zip" -o /tmp/llamacpp.zip
+unzip -j /tmp/llamacpp.zip -d llamacpp
 
 # Get a binary from our CI pipeline
-curl -L "https://github.com/StrangeBytesOrg/llama.cpp-binaries/releases/download/${LLAMA_CPP_VERSION}/llama-server-cpu.tar.gz" -o /tmp/llamacpp.tar.gz
-tar -xf /tmp/llamacpp.tar.gz -C llamacpp
+# curl -L "https://github.com/StrangeBytesOrg/llama.cpp-binaries/releases/download/${LLAMA_CPP_VERSION}/llama-server-cpu.tar.gz" -o /tmp/llamacpp.tar.gz
+# tar -xf /tmp/llamacpp.tar.gz -C llamacpp
