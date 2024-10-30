7cf3c8a5c9275d4da9a72e98209d020e4934dae2
StrangebytesDev <dev@strangebytes.org>
2024-10-30 14:42:31 -0700

    Add character search and type filtering

--- src/frontend/pages/characters.vue
+++ src/frontend/pages/characters.vue
@@ -1,11 +1,22 @@
 <script lang="ts" setup>
-import {reactive} from 'vue'
+import {reactive, ref, computed} from 'vue'
 import {RouterLink} from 'vue-router'
 import {client} from '../api-client'
 import TopBar from '../components/top-bar.vue'
 
+const searchName = ref('')
+const characterType = ref<'user' | 'character' | 'both'>('both')
 const res = await client.characters.getAll.query()
 const characters = reactive(res)
+
+// Computed property to filter characters based on search input and character type
+const filteredCharacters = computed(() => {
+    return characters.filter((character) => {
+        const matchesName = character.name.toLowerCase().includes(searchName.value.toLowerCase())
+        const matchesType = character.type === characterType.value || characterType.value === 'both'
+        return matchesName && matchesType
+    })
+})
 </script>
 
 <template>
@@ -13,9 +24,27 @@ const characters = reactive(res)
         <RouterLink to="/create-character" class="btn btn-sm btn-primary ml-auto">Create Character +</RouterLink>
     </TopBar>
 
+    <div class="flex flex-row p-3">
+        <label class="form-control w-full mr-5">
+            <input
+                type="text"
+                v-model="searchName"
+                placeholder="Search by name"
+                class="input input-bordered focus:outline-none focus:border-primary" />
+        </label>
+
+        <label class="form-control w-full">
+            <select v-model="characterType" placeholder="Character Type" class="select select-bordered">
+                <option value="both" default>Character Type</option>
+                <option value="character">Character</option>
+                <option value="user">User</option>
+            </select>
+        </label>
+    </div>
+
     <div class="flex flex-col m-2">
-        <template v-if="characters.length">
-            <div v-for="character in characters" :key="character.name">
+        <template v-if="filteredCharacters.length">
+            <div v-for="character in filteredCharacters" :key="character.name">
                 <router-link
                     :to="`/character?id=${character.id}`"
                     class="flex bg-base-200 rounded-lg p-2 mb-3 hover:outline outline-primary">
