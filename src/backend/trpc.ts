5c721872c1bf755026e457f823329c843e31304d
StrangebytesDev <dev@strangebytes.org>
2024-11-01 13:47:03 -0700

    Add context typing to trpc and add dummy userId

--- src/backend/trpc.ts
+++ src/backend/trpc.ts
@@ -2,8 +2,9 @@ import {initTRPC} from '@trpc/server'
 import {CreateFastifyContextOptions} from '@trpc/server/adapters/fastify'
 
 export function createContext({req, res}: CreateFastifyContextOptions) {
-    return {req, res}
+    const userId = 1
+    return {req, res, userId}
 }
 export type Context = Awaited<ReturnType<typeof createContext>>
 
-export const t = initTRPC.create()
+export const t = initTRPC.context<typeof createContext>().create()
