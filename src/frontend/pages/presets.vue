35a93c3608fc7443a79fc90bc181aa5e84e6a698
StrangebytesDev <dev@strangebytes.org>
2024-11-08 15:13:27 -0800

    Update preset page

--- src/frontend/pages/presets.vue
+++ src/frontend/pages/presets.vue
@@ -1,68 +1,46 @@
 <script lang="ts" setup>
-import {ref, computed} from 'vue'
+import {ref, reactive, computed} from 'vue'
 import {useToast} from 'vue-toastification'
-import {client} from '../api-client'
+import {generationPresetCollection, userCollection} from '@/db'
 import TopBar from '@/components/top-bar.vue'
 
 const toast = useToast()
 
-const res = await client.generatePresets.getAll.query()
-const presets = ref(res.presets)
-const selectedPresetId = ref(res.activePresetId)
+const presets = reactive(await generationPresetCollection.find())
+const user = reactive(await userCollection.findById('default-user'))
+const selectedPresetId = ref(user.generatePreset)
 
 const activePreset = computed(() => {
-    const missingPreset = {
-        id: 0,
-        name: '',
-        maxTokens: 0,
-        context: 0,
-        temperature: 0,
-        seed: 0,
-        minP: 0,
-        topP: 0,
-        topK: 0,
-        repeatPenalty: 0,
-        repeatLastN: 0,
-        penalizeNL: false,
-        presencePenalty: 0,
-        frequencyPenalty: 0,
-    }
-    return presets.value?.find((preset) => preset.id === selectedPresetId.value) || missingPreset
+    return presets.find((preset) => preset._id === user.generatePreset)
 })
 
 const setActivePreset = async () => {
-    await client.generatePresets.setActive.mutate(selectedPresetId.value)
+    // Check if the preset exists
+    await generationPresetCollection.findById(selectedPresetId.value)
+
+    user.generatePreset = selectedPresetId.value
+    await userCollection.update(user)
+
     toast.success('Active preset set')
 }
 
 const updatePreset = async () => {
-    await client.generatePresets.update.mutate({
-        id: selectedPresetId.value,
-        name: activePreset.value.name,
-        context: activePreset.value.context,
-        maxTokens: activePreset.value.maxTokens,
-        seed: activePreset.value.seed,
-        temperature: activePreset.value.temperature,
-        topK: activePreset.value.topK,
-        topP: activePreset.value.topP,
-        minP: activePreset.value.minP,
-        repeatPenalty: activePreset.value.repeatPenalty,
-        repeatLastN: activePreset.value.repeatLastN,
-        penalizeNL: activePreset.value.penalizeNL,
-        presencePenalty: activePreset.value.presencePenalty,
-        frequencyPenalty: activePreset.value.frequencyPenalty,
-    })
-    toast.success('Preset updated')
+    if (activePreset.value) {
+        await generationPresetCollection.update(activePreset.value)
+        toast.success('Preset updated')
+    }
 }
 
 const deletePreset = async () => {
-    await client.generatePresets.delete.mutate(selectedPresetId.value)
-    toast.success('Preset deleted')
-    presets.value.splice(
-        presets.value.findIndex((preset) => preset.id === selectedPresetId.value),
-        1,
-    )
-    selectedPresetId.value = 1
+    if (activePreset.value?._id === 'default-preset') {
+        throw new Error('Cannot delete the default preset')
+    }
+
+    await generationPresetCollection.removeById(selectedPresetId.value)
+    // Set the default preset as active
+    user.generatePreset = 'default-generation-preset'
+    selectedPresetId.value = 'default-generation-preset'
+    await userCollection.update(user)
 }
 </script>
 
@@ -73,7 +51,7 @@ const deletePreset = async () => {
         <div class="flex flex-row">
             <div class="flex flex-col">
                 <select v-model="selectedPresetId" @change="setActivePreset" class="select select-bordered min-w-60">
-                    <option v-for="preset in presets" :key="preset.id" :value="preset.id">
+                    <option v-for="preset in presets" :key="preset._id" :value="preset._id">
                         {{ preset.name }}
                     </option>
                 </select>
@@ -82,7 +60,7 @@ const deletePreset = async () => {
             <router-link to="/create-preset" class="btn btn-primary mt-auto ml-3">Create Preset</router-link>
         </div>
 
-        <div class="flex flex-col justify-between bg-base-200 rounded-lg p-3 pt-1 mt-3">
+        <div v-if="activePreset" class="flex flex-col justify-between bg-base-200 rounded-lg p-3 pt-1 mt-3">
             <div class="flex flex-row">
                 <div class="flex flex-col flex-grow mr-8">
                     <label class="form-control w-full">
@@ -209,5 +187,6 @@ const deletePreset = async () => {
                 <button @click="deletePreset" class="btn btn-error flex-grow">Delete</button>
             </div>
         </div>
+        <div v-else>Preset not found</div>
     </div>
 </template>
