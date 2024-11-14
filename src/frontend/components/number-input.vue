be58589823fea35eed17cba819102dc5f1899a63
StrangeBytesDev <dev@strangebytes.org>
2024-11-14 15:47:27 -0800

    Create number-input which returns undefined instead of empty string

---
+++ src/frontend/components/number-input.vue
@@ -0,0 +1,14 @@
+<script lang="ts" setup>
+import {defineEmits} from 'vue'
+
+defineProps<{modelValue: number | undefined}>()
+const emit = defineEmits(['update:modelValue'])
+const handleUpdate = (event: Event) => {
+    const target = event.target as HTMLInputElement
+    emit('update:modelValue', parseFloat(target.value) || undefined)
+}
+</script>
+
+<template>
+    <input type="number" :value="modelValue" @input="handleUpdate" />
+</template>
