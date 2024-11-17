d2347316c48979bff9b0e5b65008866fffcb9a60
StrangeBytesDev <dev@strangebytes.org>
2024-11-16 19:24:27 -0800

    Implement basic model downloader

--- src/frontend/pages/backend-settings.vue
+++ src/frontend/pages/backend-settings.vue
@@ -1,9 +1,9 @@
 <script lang="ts" setup>
 import {ref} from 'vue'
 import {useToast} from 'vue-toastification'
-import {ArrowPathIcon, CheckCircleIcon} from '@heroicons/vue/24/outline'
+import {ArrowPathIcon, CheckCircleIcon, ArrowDownIcon} from '@heroicons/vue/24/outline'
 import TopBar from '@/components/top-bar.vue'
-import {client} from '@/api-client'
+import {client, streamingClient} from '@/api-client'
 import {useModelStore} from '@/store'
 import NumberInput from '@/components/number-input.vue'
 
@@ -22,6 +22,11 @@ const autoLoad = ref(false)
 const gpuLayers = ref(0)
 const useFlashAttn = ref(false)
 const modelLoadPending = ref(false)
+
+// Downloading
+const hfRepo = ref('')
+const hfFile = ref('')
+const progress = ref(0)
 
 const getStatus = async () => {
     const data = await client.llamaCpp.status.query()
@@ -80,6 +85,14 @@ const setAutoLoad = async () => {
     toast.success('Autoload updated')
 }
 
+const downloadModel = async () => {
+    const iterable = await streamingClient.models.downloadModel.mutate({repo: hfRepo.value, filename: hfFile.value})
+    for await (const data of iterable) {
+        console.log(data)
+        progress.value = data.progress
+    }
+}
+
 await getStatus()
 await getModels()
 </script>
@@ -88,82 +101,112 @@ await getModels()
     <TopBar title="Models" />
 
     <div class="p-3">
-        <div class="flex flex-col">
-            <div class="flex flex-row max-w-96">
-                <label class="form-control w-full">
-                    <div class="label">
-                        <span class="label-text">Model Folder</span>
-                    </div>
-                    <div>
+        <div class="flex flex-col md:flex-row">
+            <div class="flex flex-col md:w-96">
+                <div class="flex flex-row">
+                    <label class="form-control w-full">
+                        <div class="label">
+                            <span class="label-text">Model Folder</span>
+                        </div>
                         <input type="text" class="input input-bordered w-full md:max-w-96" v-model="modelPath" />
-                    </div>
-                </label>
-                <button class="btn btn-primary btn-square mt-auto ml-3" @click="setModelDir">
-                    <CheckCircleIcon class="size-6" />
-                </button>
-            </div>
-
-            <label class="form-control w-full max-w-96 mt-3">
-                <div class="label">
-                    <span class="label-text">Context size</span>
-                    <div class="tooltip" data-tip="Controls the maximum prompt size.">
-                        <div class="badge badge-secondary badge-md">?</div>
-                    </div>
+                    </label>
+                    <button class="btn btn-primary btn-square mt-auto ml-3" @click="setModelDir">
+                        <CheckCircleIcon class="size-6" />
+                    </button>
                 </div>
-                <NumberInput class="input input-bordered w-full md:max-w-96" v-model="contextSize" />
-            </label>
 
-            <label class="form-control w-full max-w-96 mt-3">
-                <div class="label">
-                    <span class="label-text">Batch size</span>
-                </div>
-                <NumberInput class="input input-bordered w-full md:max-w-96" v-model="batchSize" />
-            </label>
-
-            <label class="form-control w-full max-w-96 mt-3">
-                <div class="label">
-                    <span class="label-text">GPU layers</span>
-                </div>
-                <NumberInput class="input input-bordered w-full md:max-w-96" v-model="gpuLayers" />
-            </label>
-
-            <label class="form-control w-full max-w-96 mt-3">
-                <div class="label">
-                    <span class="label-text">Use Flash Attention</span>
-                </div>
-                <input type="checkbox" class="toggle toggle-primary" v-model="useFlashAttn" />
-            </label>
-
-            <label class="form-control w-full max-w-96 mt-3">
-                <div class="label">
-                    <span class="label-text">Auto load model</span>
-                </div>
-                <input type="checkbox" class="toggle toggle-primary" v-model="autoLoad" @change="setAutoLoad" />
-            </label>
-
-            <div class="flex flex-row max-w-96">
                 <label class="form-control w-full mt-3">
                     <div class="label">
-                        <span class="label-text">Model</span>
+                        <span class="label-text">Context size</span>
+                        <div class="tooltip" data-tip="Controls the maximum prompt size.">
+                            <div class="badge badge-secondary badge-md">?</div>
+                        </div>
                     </div>
-                    <select v-model="selectModel" class="select select-bordered max-w-96 flex-grow">
-                        <option v-for="model in models" :key="model.name">
-                            {{ model.name }}
-                        </option>
-                        <option v-if="!models.length" disabled>No models found</option>
-                    </select>
+                    <NumberInput class="input input-bordered w-full" v-model="contextSize" />
                 </label>
-                <button @click="getModels" class="btn btn-neutral btn-square mt-auto ml-3">
-                    <ArrowPathIcon class="size-6" />
-                </button>
+
+                <label class="form-control w-full mt-3">
+                    <div class="label">
+                        <span class="label-text">Batch size</span>
+                    </div>
+                    <NumberInput class="input input-bordered w-full" v-model="batchSize" />
+                </label>
+
+                <label class="form-control w-full mt-3">
+                    <div class="label">
+                        <span class="label-text">GPU layers</span>
+                    </div>
+                    <NumberInput class="input input-bordered w-full" v-model="gpuLayers" />
+                </label>
+
+                <label class="form-control w-full mt-3">
+                    <div class="label">
+                        <span class="label-text">Use Flash Attention</span>
+                    </div>
+                    <input type="checkbox" class="toggle toggle-primary" v-model="useFlashAttn" />
+                </label>
+
+                <label class="form-control w-full mt-3">
+                    <div class="label">
+                        <span class="label-text">Auto load model</span>
+                    </div>
+                    <input type="checkbox" class="toggle toggle-primary" v-model="autoLoad" @change="setAutoLoad" />
+                </label>
+
+                <div class="flex flex-row">
+                    <label class="form-control w-full mt-3">
+                        <div class="label">
+                            <span class="label-text">Model</span>
+                        </div>
+                        <select v-model="selectModel" class="select select-bordered w-full">
+                            <option v-for="model in models" :key="model.name">
+                                {{ model.name }}
+                            </option>
+                            <option v-if="!models.length" disabled>No models found</option>
+                        </select>
+                    </label>
+                    <button @click="getModels" class="btn btn-neutral btn-square mt-auto ml-3">
+                        <ArrowPathIcon class="size-6" />
+                    </button>
+                </div>
+
+                <div class="flex flex-row mt-3 space-x-2">
+                    <!-- <button @click="getModels" class="btn btn-primary flex-grow">Refresh</button> -->
+                    <button @click="loadModel" :disabled="modelLoadPending" class="btn btn-primary flex-grow">
+                        Load
+                    </button>
+                    <button @click="unloadModel" :disabled="modelLoadPending" class="btn btn-primary flex-grow">
+                        Unload
+                    </button>
+                </div>
             </div>
 
-            <div class="flex flex-row mt-3 md:max-w-96 space-x-2">
-                <!-- <button @click="getModels" class="btn btn-primary flex-grow">Refresh</button> -->
-                <button @click="loadModel" :disabled="modelLoadPending" class="btn btn-primary flex-grow">Load</button>
-                <button @click="unloadModel" :disabled="modelLoadPending" class="btn btn-primary flex-grow">
-                    Unload
-                </button>
+            <div class="flex w-4"></div>
+
+            <!-- Download -->
+            <div class="flex flex-col md:w-96 mt-6 md:mt-0">
+                <h2>Download Models</h2>
+
+                <label class="form-control w-full mt-3">
+                    <div class="label">
+                        <span class="label-text">Huggingface Repo</span>
+                    </div>
+                    <input type="text" v-model="hfRepo" class="input input-bordered w-full" />
+                </label>
+
+                <div class="flex flex-row">
+                    <label class="form-control w-full mt-3">
+                        <div class="label">
+                            <span class="label-text">GGUF Filename</span>
+                        </div>
+                        <input type="text" v-model="hfFile" class="input input-bordered w-full" />
+                    </label>
+                    <button @click="downloadModel" class="btn btn-square btn-primary ml-2 mt-auto">
+                        <ArrowDownIcon class="size-6" />
+                    </button>
+                </div>
+
+                <div v-if="progress > 0" class="mt-3">Progress: {{ progress }}%</div>
             </div>
         </div>
     </div>
