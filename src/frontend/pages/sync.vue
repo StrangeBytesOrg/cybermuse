e1b8a07e6716de7bf3dcdede836e0d69024fb0fa
StrangeBytesDev <dev@strangebytes.org>
2025-03-11 11:33:56 -0700

    Sync up and down in one request

--- src/frontend/pages/sync.vue
+++ src/frontend/pages/sync.vue
@@ -7,12 +7,18 @@ import client from '@/sync-client'
 const toast = useToastStore()
 const syncProvider = ref('hub')
 
-const syncDown = async () => {
-    const {data: remoteDocs, error} = await client.GET('/list')
+const sync = async () => {
+    const {data: remoteDocs, error} = await client.GET('/list', {
+        headers: {
+            'Authorization': `Bearer ${localStorage.getItem('token')}`,
+        },
+    })
     if (error) {
         toast.error(`Syncing failed: ${error}`)
         return
     }
+
+    // Sync Down
     for (const doc of remoteDocs) {
         const [store, id] = doc.key.split('/')
         const collection = db.tables.find(t => t.name === store)
@@ -34,16 +40,8 @@ const syncDown = async () => {
             await collection.put(data)
         }
     }
-    toast.success('Synced down')
-}
 
-const syncUp = async () => {
-    const {data: remoteDocs, error} = await client.GET('/list')
-    if (error) {
-        toast.error(`Syncing failed: ${error}`)
-        return
-    }
-
+    // Sync up
     for (const table of db.tables) {
         const localDocs = await table.toArray()
         for (const doc of localDocs) {
@@ -61,7 +59,8 @@ const syncUp = async () => {
             }
         }
     }
-    toast.success('Synced up')
+
+    toast.success('Synced')
 }
 </script>
 
@@ -74,11 +73,8 @@ const syncUp = async () => {
                 <option value="hub">Cybermuse Hub</option>
                 <option value="self-hosted" disabled>Self Hosted</option>
             </select>
-        </div>
 
-        <div v-if="syncProvider === 'hub'" class="flex flex-row mt-3">
-            <button class="btn btn-primary" @click="syncDown">Sync Down</button>
-            <button class="btn btn-primary ml-2" @click="syncUp">Sync Up</button>
+            <button v-if="syncProvider === 'hub'" class="btn btn-primary ml-2" @click="sync">Sync</button>
         </div>
     </main>
 </template>
