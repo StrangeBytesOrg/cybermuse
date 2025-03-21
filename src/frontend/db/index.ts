69d1a874a817c3e72683cba3f9eeb9df058723b2
StrangeBytesDev <dev@strangebytes.org>
2025-03-21 00:48:48 -0700

    Add new collections to DB

--- src/frontend/db/index.ts
+++ src/frontend/db/index.ts
@@ -1,4 +1,6 @@
 import Dexie, {type EntityTable} from 'dexie'
+import {Collection} from '@/lib/dexie-orm'
+import z from 'zod'
 
 const dbName = 'cybermuse'
 
@@ -86,6 +88,86 @@ db.version(1).stores({
     templates: 'id',
     generationPresets: 'id',
 })
+
+export const characterCollection = new Collection(
+    db.characters,
+    z.object({
+        id: z.string(),
+        lastUpdate: z.number(),
+        deleted: z.number().optional(),
+        name: z.string(),
+        type: z.union([z.literal('user'), z.literal('character')]),
+        description: z.string(),
+        firstMessage: z.string().optional(),
+        avatar: z.string().optional(),
+    }),
+)
+export const loreCollection = new Collection(
+    db.lore,
+    z.object({
+        id: z.string(),
+        lastUpdate: z.number(),
+        deleted: z.number().optional(),
+        name: z.string(),
+        entries: z.array(z.object({
+            name: z.string(),
+            content: z.string(),
+        })),
+    }),
+)
+export const chatCollection = new Collection(
+    db.chats,
+    z.object({
+        id: z.string(),
+        lastUpdate: z.number(),
+        deleted: z.number().optional(),
+        name: z.string(),
+        userCharacter: z.string(),
+        characters: z.array(z.string()),
+        lore: z.array(z.string()),
+        createDate: z.string(),
+        messages: z.array(z.object({
+            id: z.string(),
+            characterId: z.string(),
+            type: z.union([z.literal('user'), z.literal('model'), z.literal('system')]),
+            content: z.array(z.string()),
+            activeIndex: z.number(),
+        })),
+        archived: z.boolean(),
+    }),
+)
+export const templateCollection = new Collection(
+    db.templates,
+    z.object({
+        id: z.string(),
+        lastUpdate: z.number(),
+        deleted: z.number().optional(),
+        name: z.string(),
+        template: z.string(),
+    }),
+)
+export const generationPresetCollection = new Collection(
+    db.generationPresets,
+    z.object({
+        id: z.string(),
+        lastUpdate: z.number(),
+        deleted: z.number().optional(),
+        name: z.string(),
+        maxTokens: z.number(),
+        temperature: z.number(),
+        seed: z.number().optional(),
+        topK: z.number().optional(),
+        topP: z.number().optional(),
+        minP: z.number().optional(),
+        repeatPenalty: z.object({
+            penalty: z.number().optional(),
+            presencePenalty: z.number().optional(),
+            frequencyPenalty: z.number().optional(),
+            lastTokens: z.number().optional(),
+            penalizeNewLine: z.boolean().optional(),
+        }),
+    }),
+)
 
 for (const table of db.tables) {
     table.hook('creating', (primKey, obj) => {
