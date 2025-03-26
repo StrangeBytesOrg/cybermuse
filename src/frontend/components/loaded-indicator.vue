f8b72c6ac661b5145224558f415d17c73fde7de0
StrangeBytesDev <dev@strangebytes.org>
2025-03-26 13:26:37 -0700

    Remove unused component

--- src/frontend/components/loaded-indicator.vue
+++
@@ -1,9 +0,0 @@
-<script lang="ts" setup>
-import {useModelStore} from '@/store'
-
-const modelStore = useModelStore()
-</script>
-
-<template>
-    <div>Model Loaded: {{ modelStore.loaded }}</div>
-</template>
