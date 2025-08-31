c4c17407a285b83962b6b2979cb7d1e1812b53ce
StrangeBytesDev <dev@strangebytes.org>
2025-08-31 00:50:20 -0700

    Add runtime flag to open dev tools

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"context"
 	"embed"
+	"flag"
 
 	"github.com/wailsapp/wails/v2"
 	"github.com/wailsapp/wails/v2/pkg/options"
@@ -21,6 +22,12 @@ func (a *App) startup(ctx context.Context) { a.ctx = ctx }
 var assets embed.FS
 
 func main() {
+	// Command-line flags to control debug behaviour.
+	debugFlag := flag.Bool("debug", false, "Open the Wails inspector (devtools) on startup")
+	debugShortFlag := flag.Bool("d", false, "Alias for --debug")
+	flag.Parse()
+	openInspector := *debugFlag || *debugShortFlag
+
 	// Create an instance of the app structure
 	app := &App{}
 
@@ -35,7 +42,7 @@ func main() {
 		BackgroundColour: &options.RGBA{R: 27, G: 38, B: 54, A: 1},
 		OnStartup:        app.startup,
 		Debug: options.Debug{
-			OpenInspectorOnStartup: true,
+			OpenInspectorOnStartup: openInspector,
 		},
 		// Bind: []interface{}{
 		// 	app,
